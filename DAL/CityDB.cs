245a8d8a42327527bae8609742a6f06b7ddd3536
GummynatorFork <adressecamionpoubellgmail.com>
2022-01-06 19:08:39 +0100

    City select picker

---
+++ DAL/CityDB.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
+
+namespace DAL
+{
+    class CityDB : ICityDB
+    {
+        private IConfiguration Configuration { get; }
+
+        public CityDB(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public List<City> GetCities()
+        {
+            List<City> results = null;
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "Select * from Cities";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    
+                    cn.Open();
+
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            if (results == null)
+                                results = new List<City>();
+
+                            City city = new City();
+
+                            city.CityId = (int)dr["CityId"];
+
+                            if (dr["Name"] != null)
+                                city.Name = (string)dr["Name"];
+
+                            if (dr["Zip"] != null)
+                                city.ZipCode = (int)dr["Zip"];                    
+
+                         
+                            results.Add(city);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return results;
+        }
+        
+    }
+}
---
+++ DAL/ICityDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DTO;
+
+namespace DAL
+{
+    public interface ICityDB
+    {
+        public List<City> GetCities();
+    }
+}
